8c2c0168d6409f54e743b809fcc293c481e3ef5c
Freddy Carrion <57779200+FreddyEcu-Ch@users.noreply.github.com>
2021-05-17 08:43:38 -0500

    Initial Commit [GTX-1]

--- Plotly Graphs/Well_Logs.py
+++ Plotly Graphs/Well_Logs.py
@@ -6,3 +6,12 @@ from plotly import tools
 import plotly.graph_objs as go
 import lasio
 
+
+# Import and read Lasio Files
+file = r"C:\Users\fredd\OneDrive - Escuela Superior Politécnica del Litoral\Escritorio\Python\Oil and Gas Data\ACAC-167.las"
+
+data = lasio.read(file)
+
+
+
+
